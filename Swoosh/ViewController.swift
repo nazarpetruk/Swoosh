1212ee33ebdf41bca9a3649efe1ef703b59b55a7
nazar petruk <nazarpetruk15@gmail.com>
2019-08-29 16:46:56 +0200

    Segues pick'n'drop

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle{
         return .lightContent
     }
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
     
 
 
