9b78b4dacc986b3e84e45641f1a2d3243d073ab8
nazar petruk <nazarpetruk15@gmail.com>
2019-08-29 14:14:18 +0200

    Initial Commit

---
+++ Swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Nazar Petruk on 29/08/2019.
+//  Copyright © 2019 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
